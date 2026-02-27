79cc8c85bee9f3ed23aa1a90430430d843da6a9e
cheshire <fantasyofwhiteness@gmail.com>
2026-02-27 02:43:01 +0100

    Version 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.cheshire.wallpaperswitcher"
         minSdk = 35
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
